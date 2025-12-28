1efb44935519cecf5a5fae048b120f1f158f4280
qhoncheh <ghonche.ataee@gmail.com>
2025-12-28 08:56:50 +0330

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next Dashboard App",
@@ -16,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${Inter} text-stone-950 bg-stone-100`}
+        className={`${geistSans.variable} ${geistMono.variable}  text-stone-950 bg-stone-100`}
       >
         {children}
       </body>
